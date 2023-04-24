6a37e73998a965b81c4e438b62690bf3f794ba2f
Анастасия <setuyaa@MacBook-Pro-Anastasia.local>
2023-04-24 16:13:46 +0300

    dgak

---
+++ hash.c
@@ -0,0 +1,22 @@
+#include "memory.h"
+#include "queue.h"
+typedef struct {
+    char* value;  //Персик
+    char* disc; // Вкусный фрукт
+    element* next; // Ссылка на петрушку/ null
+}element;
+int hash_function() {
+    int id;
+    return id;
+}
+void insert() {
+
+}
+void create_table() {
+
+}
+void free_table() {
+
+}
+void get_desc() { 
+}
